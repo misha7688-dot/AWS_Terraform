dab8f531a7167e911a4651881b0817d841089fa5
MikhailKhetagurov <misha7688@gmail.com>
2026-09-04 20:52:22 +0000

    Add Terraform configuration

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.9"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "assets" {
+  bucket = "tf-shop-assets-bucket"
+  tags = {
+    Name      = "tf-shop byka"
+    ManagedBy = "terraform"
+    Owner     = "Mikhail"
+  }
+}
